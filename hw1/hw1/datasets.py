7d6650765a7229ee9f606f542fc8e1dfefc5eeab
teich <roi.teichman@campus.technion.ac.il>
2024-06-14 15:27:22 +0300

    finish datasets.py

--- hw1/hw1/datasets.py
+++ hw1/hw1/datasets.py
@@ -150,7 +150,9 @@ class SubsetDataset(Dataset):
         #  Return the item at index + offset from the source dataset.
         #  Raise an IndexError if index is out of bounds.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        if index < 0 or index >= self.subset_len:
+            raise IndexError()
+        return self.source_dataset[index+self.offset]
         # ========================
 
     def __len__(self):
